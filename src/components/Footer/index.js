2ffd0fef14dc0e1ad3c877c93e251295046cee0e
Maria Mikhaylova <mihailova_ms@gkl-kemerovo.ru>
2023-08-04 16:54:48 +0300

    test grid

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const DivWrapper = styled("div")`
   height: 5vh;
   background-color: #f6f6f6;
-  position: relative;
+  position: fixed;
   left: 0px;
   bottom: 0px;
   width: 100vw;
