e0f8ca07215d20e1d7b0ba9fd968918984471aba
Maria Mikhaylova <mihailova_ms@gkl-kemerovo.ru>
2023-08-04 16:39:10 +0300

    test grid

--- src/components/CalendarGrid/index.js
+++ src/components/CalendarGrid/index.js
@@ -21,15 +21,15 @@ const GridWrapper = styled("div")`
 
 const CellWrapper = styled.div`
   width: 12vw;
-  height: clamp(40px, (100vw + 100vh) / 8, 72.5px);
+  height: clamp(40px, (100vw)/8, 72.5px);
   // height: 10vh;
   background-color: #ffffff;
   color: #c0c0c0;
   display: grid;
   border-right: ${(props) =>
-    props.index % 8 === 0 ? "none" : "0.1rem solid #e6e6e6"};
+    props.index % 8 === 0 ? "none" : "2px solid #e6e6e6"};
   border-top: ${(props) =>
-    props.index > 8 && props.index % 8 !== 0 ? "0.1rem solid #e6e6e6" : "none"};
+    props.index > 8 && props.index % 8 !== 0 ? "2px solid #e6e6e6" : "none"};
   @media (min-width: 740px) {
     width: 92.5px;
   }
