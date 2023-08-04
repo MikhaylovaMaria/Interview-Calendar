281f8746db3cebc67e7e954378bd0d031bdbe797
Maria Mikhaylova <mihailova_ms@gkl-kemerovo.ru>
2023-08-04 19:38:04 +0300

    footer front-size

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,13 +4,14 @@ import styled from "styled-components";
 const DivWrapper = styled("div")`
   height: 5vh;
   background-color: #f6f6f6;
-  position: relative;
+  position: fixed;
   left: 0px;
   bottom: 0px;
   width: 100vw;
   display: flex;
   justify-content: space-between;
   @media (min-width: 740px) {
+    position: relative;
     width: 740px;
   }
 `;
