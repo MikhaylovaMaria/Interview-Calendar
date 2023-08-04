2fdddaea965e7e429076d667890e97f789884a93
Maria Mikhaylova <mihailova_ms@gkl-kemerovo.ru>
2023-08-04 16:32:23 +0300

    test 5

--- src/components/Monitor/index.js
+++ src/components/Monitor/index.js
@@ -22,7 +22,7 @@ const GridWrapper = styled.div`
   height: 15vh;
   border: 2px solid #ebebeb;
   @media (min-width: 740px) {
-    width: 740x;
+    width: 740px;
     // margin-top: 0.396px;
   }
 `;
